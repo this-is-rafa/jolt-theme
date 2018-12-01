cbf1f3f1f7bc22e035a87a8ab8843af7edf9fc4c
this-is-rafa <rafa@ratradio.net>
2018-12-01 12:00:37 -0500

    Audioplayer start/stop

--- src/components/Header/AudioPlayer/AudioElement/AudioElement.js
+++ src/components/Header/AudioPlayer/AudioElement/AudioElement.js
@@ -1,16 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const audioElement = (props) => {
-  return (
-    <audio id="js-player" 
-      className="audio-player"
-      preload="metadata"
-      src="http://198.27.80.205:5220/stream/&amp;amp;type=m3u"
-    >
-      Your browser can't play this. Try <a href="https://www.getfirefox.com">Firefox browser</a>.
-      Direct link to stream: <a href="http://198.27.80.205:5220/stream/&amp;type=m3u">link</a>
-  </audio>
-  );
+class AudioElement extends Component {
+
+  render() {
+    return (
+      <audio id="js-player" 
+        className="audio-player"
+        preload="metadata"
+        src="http://198.27.80.205:5220/stream/&amp;amp;type=m3u"
+      >
+        Your browser can't play this. Try <a href="https://www.getfirefox.com">Firefox browser</a>.
+        Direct link to stream: <a href="http://198.27.80.205:5220/stream/&amp;type=m3u">link</a>
+    </audio>
+    );
+  }
 }
 
-export default audioElement;
+export default AudioElement;
--- src/components/Header/AudioPlayer/PlayBar/PlayBar.js
+++ src/components/Header/AudioPlayer/PlayBar/PlayBar.js
@@ -1,31 +1,73 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Icon from '../../../UI/Icon/Icon';
+//import AudioElement from '../AudioElement/AudioElement';
 
-const playBar = () => {
-  return (
-    <div className="play-bar">
-      <div className="play-bar__box play-bar__box--live-indicator">
-        Stream On <div className="play-bar__live-dot"></div>
+
+class PlayBar extends Component {
+
+  state = {
+    playing: false,
+    volume: true
+  }
+
+  //audio = React.createRef();
+  audioUrl = 'http://198.27.80.205:5220/stream/&amp;amp;type=m3u';
+
+  componentDidMount() {
+    this.volumeRange.addEventListener('change', this.setVolume);
+  }
+
+  setVolume = () => {
+    this.audio.volume = this.volumeRange.value / 100;
+    localStorage.setItem('apVolume', this.volumeRange.value);
+  }
+
+  playPauseHandler = () => {
+    if (this.audio.paused) {
+      let cacheBust = new Date().getTime();
+      this.audio.src = this.audioUrl + '&' + cacheBust;
+      this.audio.play();
+      this.setState({playing: true});
+    } else {
+      this.audio.pause();
+      this.audio.src = '';
+      this.setState({playing: false});
+    }
+  }
+
+  render() {
+    return (
+      <div className="play-bar">
+        <audio id="js-player" 
+          className="audio-player"
+          preload="metadata"
+          src="http://198.27.80.205:5220/stream/&amp;amp;type=m3u"
+          ref={audio => {this.audio = audio}}
+        >
+          Your browser can't play this. Try <a href="https://www.getfirefox.com">Firefox browser</a>.
+          Direct link to stream: <a href="http://198.27.80.205:5220/stream/&amp;type=m3u">link</a>
+        </audio>
+        <div className="play-bar__box play-bar__box--live-indicator">
+          Stream On <div className="play-bar__live-dot"></div>
+        </div>
+        <div onClick={this.playPauseHandler} className="play-bar__box play-bar__box--play-pause" aria-label="Play / Pause">
+          { this.state.playing ? <Icon name="stop" parentClass="play-bar" /> : <Icon name="play" parentClass="play-bar" /> }
+        </div>
+        <div id="js-live-text-box" className="play-bar__box play-bar__box--live-text">
+          <span id="js-live-text" className="play-bar__scroller">
+            Live Now: Empower Your Essence with Jill Rappaport 9-10pm
+          </span>
+        </div>
+        <div id="js-volume-box" className="play-bar__box play-bar__box--volume-box">
+          <Icon name="volume" parentClass="play-bar" />
+        </div>
+        <div className="play-bar__box play-bar__box--volume-bar-box">
+          <input ref={volumeRange => (this.volumeRange = volumeRange)} className="play-bar__volume-range" type="range" />
+        </div>
       </div>
-      <div id="js-play-pause"className="play-bar__box play-bar__box--play-pause" aria-label="Play / Pause">
-        <Icon name="play" />
-        <Icon name="stop" />
-      </div>
-      <div id="js-live-text-box" className="play-bar__box play-bar__box--live-text">
-        <span id="js-live-text" className="play-bar__scroller">
-          Live Now: Empower Your Essence with Jill Rappaport 9-10pm
-        </span>
-      </div>
-      <div id="js-volume-box" className="play-bar__box play-bar__box--volume-box">
-        <Icon name="volume" />
-        <Icon name="volume-off" />
-      </div>
-      <div className="play-bar__box play-bar__box--volume-bar-box">
-        <input id="js-volume-range" className="play-bar__volume-range" type="range" />
-      </div>
-    </div>
-  );
+    );
+  }
 
 }
 
-export default playBar;
+export default PlayBar;
