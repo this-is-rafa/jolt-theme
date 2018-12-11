23089e7b6a0bc3a02fa9bc50f25038312120366f
this-is-rafa <rafa@ratradio.net>
2018-12-11 12:10:21 -0500

    Add baseUrl to nav links

--- src/components/Header/NavMobile/NavMobile.js
+++ src/components/Header/NavMobile/NavMobile.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+const JoltSettings = window.JoltSettings;
+const baseUrl = JoltSettings.path;
+
 const NavMobile = (props) => (
   <nav className={props.open ? 'nav-over nav-over--open' : 'nav-over'} >
-    <Link to="/" className="nav-over__link">Home</Link>
-    <Link to="/" className="nav-over__link">Shows</Link>
+    <Link to={baseUrl} className="nav-over__link">Home</Link>
+    <Link to={baseUrl + 'shows'} className="nav-over__link">Shows</Link>
     <Link to="/" className="nav-over__link">Archive</Link>
     <Link to="/" className="nav-over__link">Shop</Link>
     <Link to="/" className="nav-over__link">Android App</Link>
