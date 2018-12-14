653cac03b8ae18ee72c23c2a980d41ae352ce024
this-is-rafa <rafa@ratradio.net>
2018-12-13 19:07:45 -0500

    Start scrollTextHandler

--- src/components/Header/AudioPlayer/PlayBar/PlayBar.js
+++ src/components/Header/AudioPlayer/PlayBar/PlayBar.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { findDOMNode } from 'react-dom';
 import Icon from '../../../UI/Icon/Icon';
 //import AudioElement from '../AudioElement/AudioElement';
 
@@ -15,6 +16,7 @@ class PlayBar extends Component {
 
   componentDidMount() {
     this.volumeRange.addEventListener('change', this.setVolume);
+    //setTimeout(this.scrollTextHandler, 1000);
   }
 
   setVolume = () => {
@@ -32,6 +34,57 @@ class PlayBar extends Component {
       this.audio.pause();
       this.audio.src = '';
       this.setState({playing: false});
+    }
+  }
+
+  scrollTextHandler = () => {
+    //Scrolling Text
+    const liveText = findDOMNode(this.refs.liveText);
+    console.log(liveText);
+    const liveTextBox = findDOMNode(this.refs.liveTextBox);
+    
+    let liveTextInfo = liveText.getBoundingClientRect();
+    let liveTextBoxInfo = liveTextBox.getBoundingClientRect();
+
+    let liveTextW = liveTextInfo.width;
+    let liveTextBoxW = liveTextBoxInfo.width;
+    
+
+    scrollingText(liveTextW, liveTextBoxW);
+    // window.addEventListener('resize', function() {
+    //   liveTextW = $liveText.width();
+    //   liveTextBoxW = $('#js-live-text-box').width();
+    //   scrollingText(liveTextW, liveTextBoxW);
+    // });
+
+    function scrollingText(liveTextW, liveTextBoxW) {
+      if ( liveTextW > (liveTextBoxW - 4) ) {
+          console.log(liveText);
+          let indent = 0;
+          let leftMax = true;
+
+          const scroller = liveText;
+          scroller.scroll = function() {
+            if (leftMax) {
+              if ( ( liveTextW - Math.abs(indent) ) > liveTextBoxW ) {
+                indent = indent - 1;
+                liveText.style.textIndent = indent;
+              } else {
+                leftMax = false;
+              }
+            } else {
+              if ( ( liveTextW + indent ) < liveTextW ) {
+                indent = indent + 1;
+                liveText.style.textIndent = indent;
+              } else {
+                leftMax = true;
+              }
+            }
+          }
+          console.log(liveTextBoxW + ' ' + liveTextW + ' ' + indent);
+          setInterval(scroller.scroll, 1000/24);
+
+      }
     }
   }
 
@@ -53,8 +106,8 @@ class PlayBar extends Component {
         <div onClick={this.playPauseHandler} className="play-bar__box play-bar__box--play-pause" aria-label="Play / Pause">
           { this.state.playing ? <Icon name="stop" parentClass="play-bar" /> : <Icon name="play" parentClass="play-bar" /> }
         </div>
-        <div id="js-live-text-box" className="play-bar__box play-bar__box--live-text">
-          <span id="js-live-text" className="play-bar__scroller">
+        <div ref="liveTextBox" className="play-bar__box play-bar__box--live-text">
+          <span ref="liveText" className="play-bar__scroller">
             Live Now: Empower Your Essence with Jill Rappaport 9-10pm
           </span>
         </div>
