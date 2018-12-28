489ba526f016ea8a0ecfc796eb482c5b0cf367a6
Rafa <rafa@ratradio.net>
2018-12-28 12:15:41 +0100

    Add banner image option to PostList

--- src/components/PostList/PostList.js
+++ src/components/PostList/PostList.js
@@ -5,12 +5,19 @@ class PostList extends Component {
 
   renderPosts() {
     return this.props.posts.map( (post, i) => {
+      let cardImage = '';
+      if (typeof post['_embedded'] !== 'undefined' && typeof post['_embedded']['wp:featuredmedia'] !== 'undefined') {
+        cardImage = post['_embedded']['wp:featuredmedia'][0]['media_details']['sizes']['card']['source_url'];
+      }
+      if (typeof post.acf.banner_image !== 'undefined' && post.acf.banner_image !== false){
+        cardImage = post.acf.banner_image.sizes.card;
+      }
       return(
         <div className="col-md-6 col-lg-4" key={i}>
           <Card 
             title={post.title.rendered}
             link={this.props.baseUrl + post.slug}
-            imgUrl={post['_embedded']['wp:featuredmedia'][0]['media_details']['sizes']['card']['source_url']}
+            imgUrl={cardImage}
             subtitle={post.acf.schedule_text}
           />
         </div>
