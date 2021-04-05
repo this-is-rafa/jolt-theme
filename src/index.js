944e424711c2e9e4e0d2064ec54ef7ddc3fe70d9
this-is-rafa <rafa@ratradio.net>
2021-04-05 12:37:44 -0400

    Disable service worker

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { Provider } from "react-redux";
 import store from "./store/store";
 import "./scss/style.scss";
 import App from "./App";
-import * as serviceWorker from "./serviceWorker";
+//import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <BrowserRouter>
