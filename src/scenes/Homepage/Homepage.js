cdbbfb2abf383bcc502478dd516c7e585e474d75
this-is-rafa <rafa@ratradio.net>
2022-02-24 11:28:51 -0500

    Create and add mixcloud banner.

--- src/scenes/Homepage/Homepage.js
+++ src/scenes/Homepage/Homepage.js
@@ -1,13 +1,15 @@
 import React, { Fragment } from "react";
-import UpcomingShows from "./components/UpcomingShows/UpcomingShows";
 import Banner from "./components/Banner/Banner";
+import MixcloudBanner from "./components/MixcloudBanner/MixcloudBanner";
 import Schedule from "./components/Schedule/Schedule";
 import Donate from "./components/Donate/Donate";
+import UpcomingShows from "./components/UpcomingShows/UpcomingShows";
 
 const homepage = () => (
   <Fragment>
     <Banner />
     <Schedule />
+    <MixcloudBanner />
     <Donate />
     <UpcomingShows />
   </Fragment>
